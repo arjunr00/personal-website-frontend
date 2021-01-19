d219f088979a61dec64cebc34d2ac343dfb8d31a
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2021-01-18 21:06:06 -0500

    Added entry animation to logo
    * Also rounded logo circle outline paths

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,4 @@
-import anime from 'animejs';
+import anime, { AnimeTimelineInstance } from 'animejs';
 import React, { useEffect, useRef } from 'react';
 
 import './styles/Logo.scss';
@@ -6,19 +6,80 @@ import './styles/Logo.scss';
 import LogoSvg from '../assets/logo.svg';
 
 function Logo(): JSX.Element {
+  const ANIM_MAIN_STEP_DURATION = 500;
+  const ANIM_INIT_STEP_DURATION = ANIM_MAIN_STEP_DURATION;
+  const ANIM_MAIN_START = 1.5 * ANIM_INIT_STEP_DURATION;
 
-  const animationRef = useRef<anime.AnimeInstance | null>(null);
+  const animTimelineRef = useRef<AnimeTimelineInstance | null>(null);
 
   useEffect(() => {
-    animationRef.current = anime({
-      targets: 'path',
-      strokeDashoffset: [anime.setDashoffset, 0],
-      direction: 'alternate',
-      loop: true,
+    const timeline: AnimeTimelineInstance = anime.timeline();
+
+    timeline.add({
+      easing: 'easeOutBack',
+      targets: '.fill',
+      r: [0, 33],
+      duration: ANIM_INIT_STEP_DURATION,
+      delay: anime.stagger(ANIM_INIT_STEP_DURATION / 3),
+    }, 0);
+
+    timeline.add({
       easing: 'easeInOutSine',
-      duration: 1500,
-      delay: anime.stagger(100),
-    });
+      targets: '#a_bot_outline, #a_top_outline, #r_outline',
+      strokeDashoffset: [
+        anime.setDashoffset,
+        0,
+      ],
+      duration: ANIM_INIT_STEP_DURATION,
+      delay: anime.stagger(ANIM_INIT_STEP_DURATION / 3),
+    }, ANIM_INIT_STEP_DURATION);
+
+    timeline.add({
+      easing: 'easeInSine',
+      targets: '#a_bot_arc',
+      strokeDashoffset: [
+        anime.setDashoffset,
+        (el: HTMLElement | SVGElement | null): number => {
+          return 2 * anime.setDashoffset(el);
+        },
+      ],
+      duration: ANIM_MAIN_STEP_DURATION,
+    }, ANIM_MAIN_START);
+
+    timeline.add({
+      easing: 'easeOutSine',
+      targets: '#a_top_arc',
+      strokeDashoffset: [ anime.setDashoffset, 0 ],
+      duration: ANIM_MAIN_STEP_DURATION,
+    }, ANIM_MAIN_START + ANIM_MAIN_STEP_DURATION);
+
+    timeline.add({
+      easing: 'easeOutSine',
+      targets: '#r_arc',
+      strokeDashoffset: [
+        anime.setDashoffset,
+        (el: HTMLElement | SVGElement | null): number => {
+          return 2 * anime.setDashoffset(el);
+        },
+      ],
+      duration: 2 * ANIM_MAIN_STEP_DURATION / 3,
+    }, ANIM_MAIN_START + 5 * ANIM_MAIN_STEP_DURATION / 4);
+
+    timeline.add({
+      easing: 'easeOutSine',
+      targets: '#stem',
+      strokeDashoffset: [ anime.setDashoffset, 0 ],
+      duration: 3 * ANIM_MAIN_STEP_DURATION / 4,
+    }, ANIM_MAIN_START + ANIM_MAIN_STEP_DURATION);
+
+    timeline.add({
+      easing: 'linear',
+      targets: '#a_bot_extra_fill',
+      strokeDashoffset: [ anime.setDashoffset, 0 ],
+      duration: 3 * ANIM_MAIN_STEP_DURATION / 4,
+    }, `-=${ANIM_MAIN_STEP_DURATION}`);
+
+    animTimelineRef.current = timeline;
   }, []);
 
   return(
